bec355c9cbaff3d70c513f7a450132520850be48
JF-Lalonde <lalondej88@gmail.com>
2017-10-19 23:57:31 -0600

    Re-add the CORS code after having changed webpack to production

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.locals.title = 'Quantified Self'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  next()
+})
 
 app.get('/', function(request, response) {
   response.send('Quantified Self Enpoints')
