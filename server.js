3ca409d2ef142cf5a3c5d5c631a0764cf543a0ec
JF-Lalonde <lalondej88@gmail.com>
2017-10-20 00:08:50 -0600

    Add PUT to CORS acceptable verbs list

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, GET, DELETE, OPTIONS")
   next()
 })
 
