8ff037e6489011c1f2ab61500bf88fc6ffc57366
Desislava Todorova <dslavatodorova@gmail.com>
2017-10-19 18:29:04 -0600

    add header settings to prevent localhost cors error

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const database      = require('knex')(configuration);
 app.set('port', process.env.PORT || 1234)
 app.locals.title = 'Quantified Self'
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  next();
+});
+
 app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.urlencoded({ extended: true }))
 
