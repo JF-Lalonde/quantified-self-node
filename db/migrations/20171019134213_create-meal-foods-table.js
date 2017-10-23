f6965d7a80b2df5ae6d5a5fc34922a228a2e99aa
JF-Lalonde <lalondej88@gmail.com>
2017-10-23 16:33:29 -0600

    Add on delete cascade to meal foods table

--- db/migrations/20171019134213_create-meal-foods-table.js
+++ db/migrations/20171019134213_create-meal-foods-table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
  let createQuery = (`CREATE TABLE meal_foods(
   id SERIAL PRIMARY KEY NOT NULL,
-  food_id INT REFERENCES foods(ID),
+  food_id INT REFERENCES foods(ID) ON DELETE CASCADE,
   meal_id INT REFERENCES meals(ID)
  )`)
   return knex.raw(createQuery)
